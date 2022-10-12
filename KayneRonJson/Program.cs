579222dfc488d040a9e331188f95c77a2908f963
Kael Renaud <Kmans2272@gmail.com>
2022-10-12 14:58:47 -0500

    Kanye

--- KayneRonJson/Program.cs
+++ KayneRonJson/Program.cs
@@ -1,1 +1,29 @@
-﻿
+﻿using System;
+using System.Data;
+using System.IO;
+using MySql.Data.MySqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Xml.Linq;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+using KayneRonJson;
+
+#region
+var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+string connString = config.GetConnectionString("DefaultConnection");
+IDbConnection conn = new MySqlConnection(connString);
+#endregion
+
+
+for (int i = 0; i < 5; i++)
+{
+    Quotes.Kanye();
+    Console.WriteLine();
+    Thread.Sleep(5000);
+    Quotes.Ron();
+    Console.WriteLine();
+    Thread.Sleep(5000);
+}
---
+++ KayneRonJson/Quotes.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using MySql.Data.MySqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Xml.Linq;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+
+
+
+
+namespace KayneRonJson
+{
+    public class Quotes
+    {
+
+        public static HttpClient client = new HttpClient();
+
+
+        public static void Kanye()
+        {
+
+            var kayneUrl = "https://api.kanye.rest";
+            var kanyeResponse = client.GetStringAsync(kayneUrl).Result;
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+
+            Console.WriteLine($"Kanye: \"{kanyeQuote}\"");
+
+        }
+        public static void Ron()
+        {
+
+            var ronUrl = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+            var ronResponse = client.GetStringAsync(ronUrl).Result;
+            var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+
+            Console.WriteLine($"Ron: {ronQuote}");
+
+        }
+    }
+}
